b82262dff9971818af0a353fa891510a2ca86e55
Dave O <dav3oconnor@gmail.com>
2024-07-18 00:48:02 +0100

    first commit

---
+++ src/sdl_particle_simulator.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : sdl_particle_simulator.cpp
+// Author      : David O'Connor
+// Version     : 0.001
+// Copyright   : 2024
+// Description : Small particle simulator using sdl C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+
+int main() {
+
+
+
+	return 0;
+}
