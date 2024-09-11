b611b370e0e21617eb12284660ff9d2b5cfe272b
Dave O <dav3oconnor@gmail.com>
2024-09-11 18:43:22 +0100

    added Particle class

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -13,7 +13,7 @@ namespace dave_graphics {
 Particle::Particle() {
 
 	m_x = ((2.0 * rand())/RAND_MAX) -1; // rand value from 0-1?
-	m_y = ((2.0 * rand())/RAND_MAX) -1;
+	m_y = ((2,0 * rand())/RAND_MAX) -1;
 
 }
 
--- src/Particle.h
+++ src/Particle.h
@@ -11,6 +11,7 @@
 namespace dave_graphics {
 
 struct Particle { // class and struct, in a struct the members are public by default, whereas in a class they are private
+
 	// double so values can be changed gradually and rounded to nearest position.
 	double m_x;
 	double m_y;
