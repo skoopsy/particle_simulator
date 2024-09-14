484d21b00615d63b49109d4fc26cc09b8bcf5d4f
skoopsy <38195019+skoopsy@users.noreply.github.com>
2024-09-14 13:27:21 +0100

    Feature/04 refactor screen class (#6)
    * renderer and texture working with fixed color
    
    * updated gitignore
    
    * comment edits to improve understanding
    
    * added per pixel color set loop
    
    * refactored Screen, successful build-run
    
    * moved quick event check into Screen class
    
    * added quit chekc in Screen class
    
    ---------
    
    Co-authored-by: Dave O <dav3oconnor@gmail.com>

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+//============================================================================
+// Name        : sdl_particle_simulator.cpp
+// Author      : David O'Connor
+// Version     : 0.001
+// Copyright   : 2024
+// Description : Small particle simulator using SDL C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <SDL.h>
+#include "Screen.h"
+
+int main() {
+
+	dave_graphics::Screen screen;
+
+	if(screen.init() == false) {
+		std::cout << "Error initialising SDL." << std::endl;
+	}
+
+	// --- Main game loop --- //
+	while (true) {
+		// Update Particles
+		// Draw particles
+		// Check for messages/events
+
+		// Quit check
+		if(screen.processEvents() == false ) {
+			break;
+		}
+	}
+
+	screen.close();
+
+	return 0;
+}
