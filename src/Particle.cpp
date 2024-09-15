bbebf0c97dd911ba7976d89c71b4c178d4b18ddd
Dave O <dav3oconnor@gmail.com>
2024-09-15 13:03:10 +0100

    comments to particle .cpp

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -12,9 +12,11 @@ namespace dave_graphics {
 
 Particle::Particle() {
 
+	// Set particle position
 	m_x = ((2.0 * rand())/RAND_MAX) -1; // rand value from 0-1?
 	m_y = ((2.0 * rand())/RAND_MAX) -1;
 
+	// Set particle speed
 	m_xspeed = 0.005 * ((2.0 * rand())/RAND_MAX - 1);
 	m_yspeed = 0.005 * ((2.0 * rand())/RAND_MAX - 1);
 }
@@ -28,6 +30,7 @@ void Particle::update() {
 	m_x += m_xspeed;
 	m_y += m_yspeed;
 
+	//Stop particle from leaving the boundaries
 	if(m_x < -1.0 || m_x >= 1.0) {
 		m_xspeed = -m_xspeed;
 	}
