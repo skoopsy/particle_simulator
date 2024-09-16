a5d23a3aa5acd0ed8eb2c3154779551330be8282
Dave O <dave_oc@outlook.com>
2024-09-16 14:19:22 +0100

    Feature/06 particle classes (#8)
    * refactored Screen, successful build-run
    
    * moved quick event check into Screen class
    
    * added quit chekc in Screen class
    
    * fixed Screen::close()
    
    * added Screen::update()
    
    * added Screen::setPixel and some place holder pixel setting in main
    
    * used set pixel to update colors cyclically for prototype
    
    * added Particle class
    
    * added Swarm of particles class
    
    * started prototyping swarm plotting in main
    
    * added a out-of-bounds check to Screen::getPixel()
    
    * plotted rand static particle swarm in main
    
    * added cycling colors into the swarm in main
    
    * fixed issue with swarm class
    
    ---------
    
    Co-authored-by: Dave O <dav3oconnor@gmail.com>

---
+++ src/Particle.h
@@ -0,0 +1,25 @@
+/*
+ * Particle.h
+ *
+ *  Created on: 11 Sept 2024
+ *      Author: dave
+ */
+
+#ifndef PARTICLE_H_
+#define PARTICLE_H_
+
+namespace dave_graphics {
+
+struct Particle { // class and struct, in a struct the members are public by default, whereas in a class they are private
+	// double so values can be changed gradually and rounded to nearest position.
+	double m_x;
+	double m_y;
+
+public:
+	Particle();
+	virtual ~Particle();
+};
+
+} /* namespace dave_graphics */
+
+#endif /* PARTICLE_H_ */
--- src/main.cpp
+++ src/main.cpp
@@ -9,25 +9,30 @@
 #include <iostream>
 #include <SDL.h>
 #include <math.h> // for sin function
+#include <stdlib.h> // for srand to seed rand num generator
+#include <time.h> // for seeding rand num
 
 #include "Screen.h"
+#include "Swarm.h"
+#include "Particle.h"
 
 
 int main() {
 
-	dave_graphics::Screen screen;
+	// Random number gen seed based on time value
+	srand(time(NULL));
 
+	// Initialise Sceen instance
+	dave_graphics::Screen screen;
 	if(screen.init() == false) {
 		std::cout << "Error initialising SDL." << std::endl;
 	}
 
+	// Initialise swarm of particles
+	dave_graphics::Swarm swarm;
 
 	// --- Main game loop --- //
 	while (true) {
-
-
-		// Update Particles
-		// Draw particles
 
 		// Cycle color values
 		int timeElapsed = SDL_GetTicks();
@@ -35,13 +40,17 @@ int main() {
 		unsigned char red = ((1 + sin(timeElapsed * 0.002))/2) * 255; // Using sin to cyclically change the value based on time elapsed, casts into char
 		unsigned char blue = ((1 + sin(timeElapsed * 0.003))/2) * 255; // Using sin to cyclically change the value based on time elapsed, casts into char
 
-		// Set pixel colors
-		for(int y=0; y < dave_graphics::Screen::SCREEN_HEIGHT; y++) { // for each row
-			for(int x=0; x < dave_graphics::Screen::SCREEN_WIDTH; x++) { // for each column
-				screen.setPixel(x, y, red, green, blue);
-			}
+		// Draw Particles
+		const dave_graphics::Particle * const pParticles = swarm.getParticles();
+
+		for(int i=0; i<dave_graphics::Swarm::N_PARTICLES; i++) {
+			dave_graphics::Particle particle = pParticles[i];
+
+			int x = (particle.m_x + 1) * dave_graphics::Screen::SCREEN_WIDTH/2;
+			int y = (particle.m_y + 1) * dave_graphics::Screen::SCREEN_HEIGHT/2;
+
+			screen.setPixel(x, y, red, green, blue);
 		}
-		screen.setPixel(400, 300, 255, 255, 255); // set white pixel in middle of frame
 
 		// Draw the screen
 		screen.update();
