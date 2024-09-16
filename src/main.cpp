4d1c5304fe1d90bd8fad2e231ad14ce2b63778ed
Dave O <38195019+skoopsy@users.noreply.github.com>
2024-09-16 15:36:46 +0100

    Feature/05 add set px method (#7)
    * renderer and texture working with fixed color
    
    * updated gitignore
    
    * comment edits to improve understanding
    
    * added per pixel color set loop
    
    * refactored Screen, successful build-run
    
    * moved quick event check into Screen class
    
    * fixed Screen::close()
    
    * added Screen::update()
    
    * added Screen::setPixel and some place holder pixel setting in main
    
    * used set pixel to update colors cyclically for prototype
    
    ---------

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,10 @@
 
 #include <iostream>
 #include <SDL.h>
+#include <math.h> // for sin function
+
 #include "Screen.h"
+
 
 int main() {
 
@@ -18,11 +21,30 @@ int main() {
 		std::cout << "Error initialising SDL." << std::endl;
 	}
 
+
 	// --- Main game loop --- //
 	while (true) {
+
+
 		// Update Particles
 		// Draw particles
-		// Check for messages/events
+
+		// Cycle color values
+		int timeElapsed = SDL_GetTicks();
+		unsigned char green = ((1 + sin(timeElapsed * 0.001))/2) * 255; // Using sin to cyclically change the value based on time elapsed, casts into char
+		unsigned char red = ((1 + sin(timeElapsed * 0.002))/2) * 255; // Using sin to cyclically change the value based on time elapsed, casts into char
+		unsigned char blue = ((1 + sin(timeElapsed * 0.003))/2) * 255; // Using sin to cyclically change the value based on time elapsed, casts into char
+
+		// Set pixel colors
+		for(int y=0; y < dave_graphics::Screen::SCREEN_HEIGHT; y++) { // for each row
+			for(int x=0; x < dave_graphics::Screen::SCREEN_WIDTH; x++) { // for each column
+				screen.setPixel(x, y, red, green, blue);
+			}
+		}
+		screen.setPixel(400, 300, 255, 255, 255); // set white pixel in middle of frame
+
+		// Draw the screen
+		screen.update();
 
 		// Quit check
 		if(screen.processEvents() == false ) {
